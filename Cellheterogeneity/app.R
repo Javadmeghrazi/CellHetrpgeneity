fbda07b8b4afdc225e70fd12f2d77c93bf29d011
Javad Meghrazi <meghrazijavad@gmail.com>
2022-12-07 15:10:23 -0800

    update'

--- Cellheterogeneity/app.R
+++ Cellheterogeneity/app.R
@@ -37,7 +37,9 @@ ui <- shinyUI( fluidPage(
           numericInput(inputId = "g", label = "Growth rate of the cells (g)", value = 0.5, 
                        min = 0, max = 1, step = 10),
           numericInput(inputId = "t", label = "simulation rounds (t)", value = 400, 
-                       min = 1, max = 1000, step = 1000)
+                       min = 1, max = 1000, step = 1000),
+          numericInput(inputId = "dt", label = "time-step length (dt)", value = 0.1, 
+                       min = 0.001, max = 1, step = 1000)
           
         ),
 
@@ -63,19 +65,19 @@ server <- function(input, output) {
   # g is growth rate when there is enough food. but it should be very small to be n the same time order as protein degradation/production
   # p0 is the protein production rate when there is no food
   # p1 is the increase in the protein production rate per a unit increase in cell fitness. 
-  l <- input$l; p0<-input$p0; p1<-input$p1; d<- input$d; g<-input$g; kf<-input$kf; 
-  qf<-input$qf; s<-input$s; t<-input$t; 
+  dt <- input$dt; l <- input$l; p0<-input$p0*dt; p1<-input$p1*dt; d<- input$d*dt; g<-input$g*dt; kf<-input$kf; 
+  qf<-input$qf;  t<-input$t/dt; 
   
-  #l <- 200; p0 <- 1; d <- 0.01; p1 <- 0.4; kf = 50; qf = 0; g= 0.05 ; t = 400; delta_t = 0.001
+  dt = 0.1; l <- 100; p0 <- 1*dt; d <- 0.001*dt; p1 <- 2*dt; kf = 50; qf = 1; g= 0.5*dt ; t = 40/dt; 
   # functions:
   hill <- function (j, k, q) {ifelse(q == 0, 1, j^q/(j^q+k^q))}
-  #W <- function (j){ hill (j, kf, qf)}
-  W <- function (j){ j/kf }
+  W <- function (j){ hill (j, kf, qf)}
+  #W <- function (j){ j/kf }
   Prod <- function (j) {p - j*d}
   
   #PTE_a: PTE after change. PTE_b: PTE before change
   # degredation should actually be a binomial
-  array <- expand.grid(PTE_a = 1:l, PTE_b = 1:l) %>% 
+  array <- expand.grid(PTE_a = 0:l, PTE_b = 0:l) %>% 
     mutate (degradation = ifelse(PTE_a>PTE_b, 0, dbinom(PTE_b-PTE_a, size = PTE_b, prob = d + g* W(PTE_b)))) %>% 
     mutate (production = ifelse (PTE_a<PTE_b, 0, dpois(PTE_a-PTE_b, lambda = p0 + p1*W(PTE_b)))) %>% 
     mutate (selection = ifelse(PTE_a == PTE_b, 1+g*W(PTE_b), 0))
@@ -83,26 +85,34 @@ server <- function(input, output) {
   # noise is the matrix accounting for transfer between cell states because of random events (diffusion)
   # production accounts for protein synthesis and degradation
   # selection exerts the effect of selection
-  degradation <- array$degradation; dim (degradation) <- c(l,l)
-  production <- array$production; dim(production) <- c(l,l)
-  selection <- array$selection; dim (selection) <- c(l,l)
+  degradation <- array$degradation; dim (degradation) <- c(l+1,l+1)
+  production <- array$production; dim(production) <- c(l+1,l+1)
+  selection <- array$selection; dim (selection) <- c(l+1,l+1)
   
-  freq <- rep (0, l*(t+1)); dim (freq) <- c(l,t+1)
-  freq [, 1] <- dpois (lambda = (l/4), 1:l)
+  freq <- rep (0, (l+1)*(t+1)); dim (freq) <- c(l+1,t+1)
+  freq [, 1] <- dpois (lambda = ((p0+p1*g)/(d+g)), 0:l)
   
   for (i in 2:(t+1)){
     freq [, i] <- degradation %*% production %*% selection %*% freq[, i-1] 
     freq [, i] <- freq[, i]/sum (freq [, i])
   }
   
+  r <- vector ()
+  for (i in 1: (t+1)){
+    r[i] <- sum(freq [, i] * (1 + g * W(0:l))) - 1
+  }
+  plot (r)
+  
+  
   freq <- as.data.frame(freq) %>% 
-    mutate(protein = 0:(l-1)) %>% 
+    mutate(protein = 0:(l)) %>% 
     pivot_longer(1:(t+1), values_to = "frequency", names_to = "time") %>% 
     mutate (time = as.numeric (substr(time, 2, nchar(time))))
   
   
-  vector_b <- sample (1:l, size = 100000, prob = freq %>% filter (time == 1) %>% select (frequency) %>% unlist(), replace = TRUE )
-  vector_a <- sample (1:l, size = 100000, prob = freq %>% filter (time == t) %>% select (frequency) %>% unlist(), replace = TRUE )
+  
+  vector_b <- sample (0:l, size = 100000, prob = freq %>% filter (time == 1) %>% select (frequency) %>% unlist(), replace = TRUE ) 
+  vector_a <- sample (0:l, size = 100000, prob = freq %>% filter (time == t) %>% select (frequency) %>% unlist(), replace = TRUE )
   
   df <- data.frame (prot_b = vector_b, prot_a = vector_a) %>% 
     pivot_longer(cols = 1:2, names_to = "when", values_to = "conc") %>% 
@@ -110,10 +120,11 @@ server <- function(input, output) {
     pivot_longer(cols = c("conc", "log_conc"), names_to = "log", values_to = "conc")
   
   ggplot (df, aes(x = conc))+
-    geom_histogram(data = df %>% filter(when == "prot_b"), fill = "red", alpha = 0.2, bins = l/2)+
-    geom_histogram(data = df %>% filter(when == "prot_a"), fill = "green", alpha = 0.2,bins = l/2)+
+    geom_histogram(data = df %>% filter(when == "prot_b"), fill = "red", alpha = 0.2, bins = (l+1))+
+    geom_histogram(data = df %>% filter(when == "prot_a"), fill = "green", alpha = 0.2,bins = (l+1))+
     facet_wrap(~ log, scales = "free")+
-    theme_classic()
+    theme_classic()+
+    labs (caption = paste ("final_growth_rate =", round ( r[t+1], digits = 4)))
   })   
   
 
@@ -121,4 +132,5 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-  
+
+# I found what's going wrong. probability in binom dist becomes larger than 1. I can fix it by dt  
--- enzymes.R
+++ enzymes.R
@@ -3,18 +3,18 @@ lapply (pkgs, library, character.only = TRUE)
 # parameters ------------------
 
 # I'm assuming that the number of proteins in proteome is 4 times as much as the maximum number of PTE that we've observed. 
-proteome = max(PTE_enz$enzymes_per_cell, na.rm = TRUE) * 4
+proteome = 5e6
 
 # toxicity costs for aggregated proteins (tox is the linear cost and tox 2 is the quadatic cost)
-tox  = 1 / proteome * 1
-tox2 = 1 / proteome^2 *1000
+tox  = 1 / proteome * 0.5
+tox2 = 1 / proteome^2 *100
 
 
 
 # calculations --------------
 PTE_enz <- read.csv ("PTE_data/PTE enzyme number and growth rate.csv", strip.white = TRUE) %>% 
-  rename (km = KM..M.1., kcat = kcat..s.1., mean_r = mean_growthrate..per.hour., solubility = solubility...) %>% 
-  mutate (solubility = solubility / 100)
+  mutate (solubility = solubility / 100, free_proteome = (proteome - mean_enzymes_per_cell), aggregate = mean_enzymes_per_cell * (1 - solubility)) %>% 
+  filter (mean_enzymes_per_cell < 1e7 , genotype != "R1")
 
 # enzymatic activity is calculated with 2 different assumptions about enzyme saturation with substrate in the cytoplasm
 # growth rate is assumed to have a linear relationship with log(activity)
@@ -24,29 +24,122 @@ PTE_enz <- read.csv ("PTE_data/PTE enzyme number and growth rate.csv", strip.whi
 # it's assumed that the cost of protein production is linearly dependent on growth rate
 # it's assumed that the cost of toxicity is independent of the growth rate 
 
-PTE_enz <- PTE_enz %>% mutate (act_unsaturated = kcat * enzymes_per_cell * solubility / km ) %>% 
-  mutate (act_saturated = kcat * enzymes_per_cell * solubility) %>% 
+PTE_enz <- PTE_enz %>% mutate (act_unsaturated = kcat * mean_enzymes_per_cell * solubility / km ) %>% 
+  mutate (act_saturated = kcat * mean_enzymes_per_cell * solubility) %>% 
   pivot_longer(cols = c(act_saturated, act_unsaturated), names_to = "s_assump", values_to = "activity") %>% 
   mutate (activity = abs (activity),
           r_notox = ifelse (Psource == "Px",
-                            log (activity) * (proteome - enzymes_per_cell)/proteome,
-                            (proteome - enzymes_per_cell)/proteome ),
-          r_tox = r_notox - tox * enzymes_per_cell * (1-solubility),
-          r_tox2 = r_tox - tox2 * (enzymes_per_cell * (1-solubility))^2) %>% 
+                            log (activity) * free_proteome/proteome,
+                            free_proteome/proteome ),
+          r_tox = r_notox - tox * mean_enzymes_per_cell * (1-solubility),
+          r_tox2 = r_tox - tox2 * (mean_enzymes_per_cell * (1-solubility))^2) %>% 
   pivot_longer(cols = c(r_notox, r_tox, r_tox2), names_to = "toxicity", values_to = "r_expected")
 
 # plots ----------------------
-# plotting the mean_r over log(actiyity). It's more or less linear
-ggplot (data = PTE_enz, aes (x = log(activity), y = mean_r) )+
+# plotting the mean_growthrate over log(actiyity). It's more or less linear
+# the unsaturated version sounds more reasonable to me because those 3 outliers are closer to other points (not a solid reason)
+ggplot (data = PTE_enz, aes (x = log(activity), y = mean_growthrate) )+
   geom_point()+
   facet_grid(Psource ~ s_assump, scales = "free_x")
 
-# plotting the mean_r over r_expected for non selective media
-ggplot (data = PTE_enz %>% filter(Psource == "Pi"), aes (x = r_expected, y = mean_r) )+
+# plotting mean_growthrate over percentage of proteome not occupied by PTE
+ggplot (data = PTE_enz, aes (x = free_proteome/proteome, y = mean_growthrate) )+
   geom_point()+
+  facet_wrap(~Psource , scales = "free_x")
+
+
+# plotting mean_enzymes_per_cell over solubility 
+# seems like there is a small decrease
+ggplot (data = PTE_enz %>% filter (Psource == "Pi"), aes (x = (1-solubility), y = mean_enzymes_per_cell) )+
+  geom_text(aes(label=genotype))+
+  facet_grid(s_assump~Psource , scales = "free_y")
+
+# plotting mean_growthrate over solubility 
+# seems like there is a small decrease
+ggplot (data = PTE_enz, aes (x = (1-solubility), y = mean_growthrate) )+
+  geom_text(aes(label=genotype))+
+  facet_grid(s_assump~Psource , scales = "free_x")
+
+# plotting mean_growthrate over aggregate
+# seems like there is a small decrease
+ggplot (data = PTE_enz , aes (x = aggregate, y = mean_growthrate) )+
+  geom_text(aes(label=genotype))+
+  facet_wrap(~Psource , scales = "free_x")
+
+
+# plotting the mean_growthrate over r_expected for non selective media
+ggplot (data = PTE_enz %>% filter(Psource == "Pi"), aes (x = r_expected, y = mean_growthrate) )+
+  geom_text(aes(label=genotype))+
   facet_grid(s_assump ~ toxicity)
 
-# plotting the mean_r over r_expected for selective media
-ggplot (data = PTE_enz %>% filter(Psource == "Px"), aes (x = r_expected, y = mean_r) )+
-  geom_point()+
+# plotting the mean_growthrate over r_expected for selective media
+ggplot (data = PTE_enz %>% filter(Psource == "Px"), aes (x = r_expected, y = mean_growthrate) )+
+  geom_text(aes(label=genotype))+
   facet_grid(s_assump ~ toxicity)
+
+ggplot (data = PTE_enz , aes (x = (1-solubility), y = log(kcat)) )+
+  geom_text(aes(label=genotype))+
+  facet_wrap(~Psource)
+
+ggplot (data = PTE_enz , aes (x = (1-solubility), y = log(mean_enzymes_per_cell)) )+
+  geom_text(aes(label=genotype))+
+  facet_wrap(~Psource)
+
+# linear models -----------------
+# I'm gonna use lm to determine the realtive importance of different factors
+# order of terms influence the results 
+PTE_enz <- PTE_enz %>% mutate (log_activity = log ( activity))
+Px_saturated <- lm (data = PTE_enz %>% filter (Psource == "Px", s_assump == "act_saturated"), mean_growthrate ~ free_proteome + aggregate + activity)
+
+Px_saturated <- lm (data = PTE_enz %>% filter (Psource == "Px", s_assump == "act_saturated"), mean_growthrate ~ mean_enzymes_per_cell * solubility * log_activity)
+anova (Px_saturated)
+
+Px_saturated_int <- lm (data = PTE_enz %>% filter (Psource == "Px", s_assump == "act_saturated"), mean_growthrate ~ free_proteome * aggregate * activity)
+anova (Px_saturated_int)
+
+Px_unsaturated <- lm (data = PTE_enz %>% filter (Psource == "Px", s_assump == "act_unsaturated"), mean_growthrate ~ free_proteome + aggregate + activity)
+anova (Px_unsaturated)
+
+Px_unsaturated_int <- lm (data = PTE_enz %>% filter (Psource == "Px", s_assump == "act_unsaturated"), mean_growthrate ~ free_proteome * aggregate * activity)
+anova (Px_unsaturated_int)
+
+# AIC is used to find the best model:
+
+
+# checking if we are cool! We are
+ggplot (data = PTE_enz, aes ( x = log(mean_meanV), y = log (activity)))+
+  geom_point()+
+  facet_grid(Psource ~ s_assump)
+
+# does the number of of enzymes per cell increase when enzyme is less stable? (Pi is more reliable)
+ggplot (data = PTE_enz, aes(x = (1-solubility), y = log( mean_enzymes_per_cell)))+
+  geom_text(aes(label=genotype))+
+  facet_wrap(~Psource) # the results in PI suggest that aggregation does not cause the cell to accumulate more of PTE. 
+# But the positive correlation in Px tells that cells are under selection to have certain activity of PTE and if enzyme is less soluble, selection results in higher number of enzymes
+
+# does the number of enzymes per cell change with the activity of the enzymes?
+ggplot (data = PTE_enz, aes(x = log (kcat), y = log( mean_enzymes_per_cell)))+
+  geom_point()+
+  facet_wrap(~Psource) # there is a bit of correlation in Px. higher the kcat, lower the number of enzymes
+# the same analysis but assuming the enzyme is not saturated
+ggplot (data = PTE_enz, aes(x = log (kcat/km), y = log( mean_enzymes_per_cell)))+
+  geom_point()+
+  facet_wrap(~Psource) #similar to the previous graph. I should do the stats to be able to compare them. 
+
+# The cells that have so many enzymes are the cells that are not growing well. they probably don't have enough enzymatic activity and have failed to produce more of the PTE. 
+ggplot (data = PTE_enz, aes(x= log( mean_enzymes_per_cell), y = mean_growthrate))+
+  geom_point()+
+  facet_wrap(~Psource)
+
+# cells with high kcat don't need to make so many enzymes
+ggplot (data = PTE_enz, aes(x= log(kcat), y = mean_growthrate))+
+  geom_point()+
+  facet_wrap(~Psource)
+# same thing with assumption of unstaturation
+ggplot (data = PTE_enz, aes(x= log(kcat/km), y = mean_growthrate))+
+  geom_point()+
+  facet_wrap(~Psource)
+
+# Sally's suggestions:
+# 1- path analysis to distinguish between direct and indirect effects of total enzyme per cell: https://rpubs.com/tbihansk/302732
+# 2- model adeqact checking: http://www.stat.columbia.edu/~gelman/research/published/ecological.pdf
